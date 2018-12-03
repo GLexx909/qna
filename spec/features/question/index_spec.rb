9a22a7ae17facefa346dd5cf74d9c2036d780148
Vitaly Simonov <guardian909@rambler.ru>
2018-12-03 18:34:48 +0300

    Realize: "User can see all questions by another users (questions list)" 	edit:      app/views/layouts/application.html.slim 	edit:      app/views/questions/index.html.slim 	edit:      spec/support/feature_helpers.rb
    	app/views/questions/_question.html.slim = render @questions
    	app/views/shared/_nav.html.slim add authentication links
    	spec/features/question/index_spec.rb acceptance test to display a list of all questions

---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+feature 'User can see questions list', %q{
+  In order to get answer for any question
+  I'd like to be able to see all questions
+} do
+
+  given(:user1) {create(:user) }
+  given(:user2) {create(:user) }
+
+  scenario 'User can see questions list' do
+    # user1 sign_in, create a question, sign_out
+    sign_in(user1)
+    visit questions_path
+    click_on 'Ask question'
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+    click_on 'Ask'
+
+    sign_out #user1
+
+    # user2 sign_in, create a question, see all questions by another users
+    sign_in(user2)
+    visit questions_path
+    click_on 'Ask question'
+
+    fill_in 'Title', with: 'Test 2 question'
+    fill_in 'Body', with: 'text 2 text text'
+    click_on 'Ask'
+    click_on 'QNA'
+
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content 'text text text'
+    expect(page).to have_content 'Test 2 question'
+    expect(page).to have_content 'text 2 text text'
+  end
+end
