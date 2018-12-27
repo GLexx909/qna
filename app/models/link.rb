d92cb59097fcf44ebb3b5557b619f4425af8b569
Vitaly Simonov <guardian909@rambler.ru>
2018-12-27 18:06:58 +0300

    perf(link) Correct incoming link (http:// or not)

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,10 @@ class Link < ApplicationRecord
   belongs_to :linkable, polymorphic: true
 
   validates :name, :url, presence: true
+
+  before_save :to_correct
+
+  def to_correct
+    self.url = /^http/i.match(self.url) ? url : "http://#{url}"
+  end
 end
