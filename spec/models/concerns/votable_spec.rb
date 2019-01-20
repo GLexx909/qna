cdf4fa49aba31c75a1708d6608da3c9a412f3ddd
Vitaly Simonov <guardian909@rambler.ru>
2019-01-20 13:49:39 +0300

    test(voted) model test

--- spec/models/concerns/votable_spec.rb
+++ spec/models/concerns/votable_spec.rb
@@ -7,6 +7,7 @@ shared_examples_for 'WithVotable' do
       create(:vote, votable: votable, user: user, value: -1)
       create(:vote, votable: votable, user: user, value: -1)
     end
+
     it 'should to give correct rating' do
       expect(votable.rating).to eq -1
     end
@@ -14,15 +15,22 @@ shared_examples_for 'WithVotable' do
 
   describe '#vote_up' do
     let(:user2) { create :user }
+
     it 'should create vote cause it not exists' do
       votable.vote_up(user2)
       expect(votable.votes.count).to eq 1
+    end
+
+    it 'should create vote cause it not exists with value "1"' do
+      votable.vote_up(user2)
+      expect(votable.votes.first.value).to eq 1
     end
 
     context 'if vote already exists with value "-1"' do
       before do
         create(:vote, votable: votable, user: user2, value: -1)
       end
+
       it 'should delete vote' do
         votable.vote_up(user2)
         expect(votable.votes.count).to eq 0
@@ -32,15 +40,22 @@ shared_examples_for 'WithVotable' do
 
   describe '#vote_down' do
     let(:user2) { create :user }
+
     it 'should create vote cause it not exists' do
       votable.vote_down(user2)
       expect(votable.votes.count).to eq 1
+    end
+
+    it 'should create vote cause it not exists with value "-1"' do
+      votable.vote_down(user2)
+      expect(votable.votes.first.value).to eq -1
     end
 
     context 'if vote already exists with value "1"' do
       before do
         create(:vote, votable: votable, user: user2, value: 1)
       end
+
       it 'should delete vote' do
         votable.vote_down(user2)
         expect(votable.votes.count).to eq 0
