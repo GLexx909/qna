baf2f127f545cd6c4b0dc04ea8da1ee112fc7dca
Vitaly Simonov <guardian909@rambler.ru>
2019-01-24 14:06:01 +0300

    feat(comment) Add association to user. Add model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :answers, foreign_key: 'author_id'
   has_many :questions, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
   has_many :votes, dependent: :destroy
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should have_many(:questions).with_foreign_key('author_id') }
   it { should have_many(:answers).with_foreign_key('author_id') }
+  it { should have_many(:comments).with_foreign_key('author_id') }
   it { should have_many(:votes).dependent(:destroy) }
 
   it { should validate_presence_of :email }
