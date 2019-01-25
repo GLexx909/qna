b75ca28d922c1743b3ddf2f59fdc4c3ed400f4ac
Vitaly Simonov <guardian909@rambler.ru>
2019-01-25 16:58:29 +0300

    test(comment) Add test for comments of answer

--- spec/features/comment/create_spec.rb
+++ spec/features/comment/create_spec.rb
@@ -20,15 +20,11 @@ feature 'User can add comments to question', %q{
     end
   end
 
-  scenario 'Unauthorised user tries to can not add a comment to question', js: true do
-    sign_in(user)
+  scenario 'Unauthorised user can not add a comment to question', js: true do
     visit question_path(question)
 
     within ('.question-comments-block') do
-      click_on 'Comment this'
-      fill_in 'Body', with: 'text of comment'
-      click_on 'Post Your Comment'
-      expect(page).to have_content 'text of comment'
+      expect(page).to_not have_link 'Comment this'
     end
   end
 
@@ -46,6 +42,63 @@ feature 'User can add comments to question', %q{
       Capybara.using_session('user') do
         click_on 'Comment this'
         within('.question-comment-form') do
+          fill_in 'Body', with: 'text text text'
+          click_on 'Post Your Comment'
+        end
+
+        expect(page).to have_content 'text text text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'text text text'
+      end
+    end
+  end
+end
+
+feature 'User can add comments to answer', %q{
+  In order to provide additional info to answer
+  As a registered user
+  I'd like to be able to add comment
+}do
+  given(:user) { create :user}
+  given(:question) { create :question, author: user}
+  given!(:answer) { create :answer, author: user, question: question}
+
+  scenario 'User adds comment to answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within ('.answers') do
+      click_on 'Comment this'
+      fill_in 'Body', with: 'text of comment'
+      click_on 'Post Your Comment'
+      expect(page).to have_content 'text of comment'
+    end
+  end
+
+  scenario 'Unauthorised user can not add a comment to answer', js: true do
+    visit question_path(question)
+
+    within ('.answers') do
+      expect(page).to_not have_link 'Comment this'
+    end
+  end
+
+  context 'multiple session', js: true do
+    scenario "comment appears on another user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        within('.answers') do
+          click_on 'Comment this'
           fill_in 'Body', with: 'text text text'
           click_on 'Post Your Comment'
         end
--- spec/features/comment/delete_spec.rb
+++ spec/features/comment/delete_spec.rb
@@ -6,6 +6,60 @@ feature 'User can delete comment of question', %q{
 }do
   given(:user) { create :user}
   given(:question) { create :question, author: user}
+  given!(:answer) { create :answer, author: user, question: question}
+  given!(:comment) { create :comment, author: user, commentable: answer, body: 'text of comment'}
+
+  scenario 'User delete comment of answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    within ('.answers') do
+      click_on 'Delete comment'
+      expect(page).to have_content 'text of comment'
+    end
+  end
+
+  scenario 'Unauthorised user can not delete a comment of answer', js: true do
+    visit question_path(question)
+
+    within ('.answers') do
+      expect(page).to_not have_link 'Delete Comment'
+    end
+  end
+
+  context 'multiple session', js: true do
+    scenario "comment appears on another user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+        expect(page).to have_content 'text of comment'
+      end
+
+      Capybara.using_session('user') do
+        click_on 'Delete comment'
+
+        expect(page).to_not have_content 'text of comment'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to_not have_content 'text of comment'
+      end
+    end
+  end
+end
+
+require 'rails_helper'
+
+feature 'User can delete comment of question', %q{
+  As a registered user and author
+  I'd like to be able to delete comment
+}do
+  given(:user) { create :user}
+  given(:question) { create :question, author: user}
   given!(:comment) { create :comment, author: user, commentable: question, body: 'text of comment'}
 
   scenario 'User delete comment of question', js: true do
