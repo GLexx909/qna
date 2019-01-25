5a019b039c52ec45fefb96af1ff1412f454a7c9c
Vitaly Simonov <guardian909@rambler.ru>
2019-01-25 13:46:19 +0300

    test(comment) Acceptance test comment create

--- spec/features/comment/create_spec.rb
+++ spec/features/comment/create_spec.rb
@@ -31,4 +31,31 @@ feature 'User can add comments to question', %q{
       expect(page).to have_content 'text of comment'
     end
   end
+
+  context 'multiple session', js: true do
+    scenario "comment appears on another user's page" do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+      end
+
+      Capybara.using_session('guest') do
+        visit question_path(question)
+      end
+
+      Capybara.using_session('user') do
+        click_on 'Comment this'
+        within('.question-comment-form') do
+          fill_in 'Body', with: 'text text text'
+          click_on 'Post Your Comment'
+        end
+
+        expect(page).to have_content 'text text text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'text text text'
+      end
+    end
+  end
 end
