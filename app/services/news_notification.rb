966d53b2e3fd9876308889832fdcc456941b3cd4
Vitaly Simonov <guardian909@rambler.ru>
2019-02-14 13:11:38 +0300

    ref(Sevice::NewsNotification) find_each

--- app/services/news_notification.rb
+++ app/services/news_notification.rb
@@ -2,7 +2,7 @@ class Services::NewsNotification
   def send_question_updates(answer)
     subscribers = answer.question.subscribers
 
-    subscribers.each do |user|
+    subscribers.find_each(batch_size: 500) do |user|
       NewsNotificationMailer.notify(user, answer).deliver_later
     end
   end
