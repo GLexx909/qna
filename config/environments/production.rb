56e8657b040a7d1b72e4d87dce42e7e3b8682eba
Vitaly Simonov <guardian909@rambler.ru>
2019-02-28 19:31:48 +0300

    mail from

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qna_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  # config.action_mailer.default_options = {from: 'info@qnathink2019.ru'}
+  config.action_mailer.default_options = {from: 'info@qnathink2019.ru'}
   config.action_mailer.default_url_options = { host: 'qnathink2019.ru' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
